812606a8a5d822b6da6f06018fcc25a614219af3
CamTor97 <cami.tor9710@gmail.com>
2025-11-01 18:47:01 -0500

    feat(models)Creation of the Tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name")
+    due_date = models.DateField(default=date.today, verbose_name="Due Date")
+    is_completed = models.BooleanField(default=False, verbose_name="Completed")
+
+    def __str__(self):
+        return self.name
